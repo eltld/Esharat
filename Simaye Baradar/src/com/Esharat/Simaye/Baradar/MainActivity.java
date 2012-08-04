02d0d645bfc1f3f46ce5e80cac3ae890bf7ed4c4
Mohammad Reza <programmer.net2009@live.com>
2012-08-04 13:23:00 +0430

    Main Page

---
+++ Simaye Baradar/src/com/Esharat/Simaye/Baradar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.Esharat.Simaye.Baradar;
+
+import com.esharat.simaye.baradar.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
